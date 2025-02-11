d320caa29d15f317824d9a606342872d14231b86
Colin Aydt <128675732+Colin95237@users.noreply.github.com>
2025-02-11 14:45:09 +0100

    Add files via upload

--- SpaceshipGame/code/main.py
+++ SpaceshipGame/code/main.py
@@ -3,6 +3,12 @@ from os.path import join
 
 from random import randint
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+
 #general setup
 pygame.init()
 WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
@@ -16,12 +22,14 @@ surf = pygame.Surface((100,200))
 surf.fill('orange')
 x = 100
 
+all_sprites = pygame.sprite.Group()
+player = Player(all_sprites)
+
 # imports
 player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 player_direction = pygame.math.Vector2(1, 1)
-player_speed = 1000
-
+player_speed = 500
 
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 star_positions = [(randint(0, WINDOW_WIDTH),randint(0, WINDOW_HEIGHT)) for i in range(20)]
@@ -40,6 +48,17 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    #input
+    #keys = pygame.key.get_pressed()
+    #player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    #player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    #player_direction = player_direction.normalize() if player_direction else player_direction
+    #player_rect.center += player_direction * player_speed * dt
+
+    recent_keys = pygame.key.get_just_pressed()
+    if recent_keys[pygame.K_SPACE]:
+        print('fire laser')
+
     #draw the game
     display_surface.fill('darkgray')
     for pos in star_positions:
@@ -47,14 +66,8 @@ while running:
     
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
-    
-    #player movement
-    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-        player_direction.y *= -1
-    if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-        player_direction.x *= -1    
-    player_rect.center += player_direction * player_speed * dt
-    display_surface.blit(player_surf, player_rect.topleft)
+    # display_surface.blit(player_surf, player_rect)
+    all_sprites.draw(display_surface)
 
     pygame.display.update()       
 
