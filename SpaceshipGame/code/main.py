6360423ac326c3e680a9932c26c335aaf826cf5f
Colin Aydt <128675732+Colin95237@users.noreply.github.com>
2025-02-12 11:07:26 +0100

    Add files via upload

--- SpaceshipGame/code/main.py
+++ SpaceshipGame/code/main.py
@@ -8,6 +8,25 @@ class Player(pygame.sprite.Sprite):
         super().__init__(groups)
         self.image = pygame.image.load(join('images', 'player.png')).convert_alpha()
         self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.Vector2()
+        self.speed = 300
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
+
+        recent_keys = pygame.key.get_just_pressed()
+        if recent_keys[pygame.K_SPACE]:
+            print('fire laser')
+
+class Star(pygame.sprite.Sprite):
+    def __init__(self, groups, surf):
+        super().__init__(groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center = (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
 
 #general setup
 pygame.init()
@@ -23,22 +42,21 @@ surf.fill('orange')
 x = 100
 
 all_sprites = pygame.sprite.Group()
+star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
+for i in range(20):
+        Star(all_sprites, star_surf)
 player = Player(all_sprites)
 
-# imports
-player_surf = pygame.image.load(join('images', 'player.png')).convert_alpha()
-player_rect = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(1, 1)
-player_speed = 500
-
-star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
-star_positions = [(randint(0, WINDOW_WIDTH),randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
 meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
 meteor_rect = meteor_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
 
 laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
 laser_rect = laser_surf.get_frect(bottomleft = (20, WINDOW_HEIGHT - 20))
+
+# custom events -> meteor event
+meteor_event = pygame.event.custom_type()
+
 
 
 while running:
@@ -48,25 +66,14 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    #input
-    #keys = pygame.key.get_pressed()
-    #player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
-    #player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
-    #player_direction = player_direction.normalize() if player_direction else player_direction
-    #player_rect.center += player_direction * player_speed * dt
-
-    recent_keys = pygame.key.get_just_pressed()
-    if recent_keys[pygame.K_SPACE]:
-        print('fire laser')
+    #update
+    all_sprites.update(dt)
 
     #draw the game
     display_surface.fill('darkgray')
-    for pos in star_positions:
-        display_surface.blit(star_surf, pos)
     
     display_surface.blit(meteor_surf, meteor_rect)
     display_surface.blit(laser_surf, laser_rect)
-    # display_surface.blit(player_surf, player_rect)
     all_sprites.draw(display_surface)
 
     pygame.display.update()       
