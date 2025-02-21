34fc8d0370f51a84d2f91c7df235ecabffe9d3c2
Colin Aydt <128675732+Colin95237@users.noreply.github.com>
2025-02-21 09:44:31 +0100

    Add files via upload

--- SpaceshipGame/code/main.py
+++ SpaceshipGame/code/main.py
@@ -93,10 +93,17 @@ def collisions():
         if collided_sprites:
             laser.kill()   
 
+def display_score():
+    current_time = pygame.time.get_ticks()
+    text_surf = font.render(str(current_time), True, (240,240,240))
+    text_rect = text_surf.get_rect(midbottom = (WINDOW_WIDTH / 2, WINDOW_HEIGHT - 50))
+    display_surface.blit(text_surf, text_rect) 
+
 #import
 star_surf = pygame.image.load(join('images', 'star.png')).convert_alpha()
 meteor_surf = pygame.image.load(join('images', 'meteor.png')).convert_alpha()
 laser_surf = pygame.image.load(join('images', 'laser.png')).convert_alpha()
+font = pygame.font.Font(join('images', 'Oxanium-Bold.ttf'), 20)
 
 #sprites
 all_sprites = pygame.sprite.Group()
@@ -125,8 +132,9 @@ while running:
     collisions()
 
     #draw the game
-    display_surface.fill('darkgray')
+    display_surface.fill('#3a2e3f')
     all_sprites.draw(display_surface)
+    display_score()
 
     pygame.display.update()       
 
